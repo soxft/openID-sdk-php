3e20ae06c135d6d7386b52f8c75fd46cd1f50cd1
xcsoft <email@xcsoft.top>
2021-09-14 20:30:13 +0800

    first

---
+++ xopenid-sdk.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * SDK FOR X openID 
+ * @version v1.0
+ * @lang php
+ * @author xcsoft <contact@xcsoft.top>
+ */
+
+namespace Xsdk;
+
+class XopenId
+{
+    private static $api = 'https://9420.ltd/';
+
+    /**
+     * @param string $appid 应用ID
+     * @param string $app_secret 应用密钥
+     */
+    function __construct(string $appid, string $app_secret)
+    {
+        $this->appid = $appid;
+        $this->app_secret = $app_secret;
+    }
+
+    /**
+     * 跳转至授权界面
+     * @param string $redirect_uri 回调链接
+     */
+    public function Jump(string $redirect_uri)
+    {
+        header("Location: " . self::$api . 'v1/connect.php?appid=' . $this->appid . '&redirect_uri=' . urlencode($redirect_uri));
+    }
+
+    /**
+     * 请求用户信息
+     * @param string $token GET参数中的token
+     * @return array 用户信息
+     */
+    public function getUserInfo(string $token): array
+    {
+        if (empty($token)) return ['code' => 101, 'msg' => 'token cant be empty', 'data' => []];
+        $param = [
+            'appid' => $this->appid,
+            'app_secret' => $this->app_secret,
+            'token' => $token,
+        ];
+        $res = $this->httpPost(self::$api . 'v1/userInfo.php', $param);
+        if (empty($res)) return ['code' => 102, 'msg' => 'http request error', 'data' => []];
+        return json_decode($res, true);
+    }
+
+    private function httpPost(string $url, array $param)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+}
